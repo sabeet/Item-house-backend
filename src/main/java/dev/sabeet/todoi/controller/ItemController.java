b83aded1cdc716566b64d9df49f772f9dbcf0a41
sabeet <sabeet.other.way@gmail.com>
2023-12-10 12:03:58 -0500

    Added Dockerfile and CORS

--- src/main/java/dev/sabeet/todoi/controller/ItemController.java
+++ src/main/java/dev/sabeet/todoi/controller/ItemController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class ItemController {
 
